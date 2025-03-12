c64d2854bd4586bad1071d4786f339213d754bce
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-03-12 14:39:00 +0300

    Learing the useEffect Hook

--- react-states/src/App.jsx
+++ react-states/src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+ const [color,setColor]=useState('red')
+
+ const changeColor=()=>{
+  setColor("blue")
+ }
+ const [car, setCar]=useState({
+  color:'black',
+  brand:'ferrari',
+  country:'Germany',
+  model:'M34',
+ })
+
+const switchCar=()=>{
+  setCar((prev)=>{
+    return{
+      ...prev,
+    color:'red',
+    }
+  })
+}
+const [Count,setCount]=useState(0)
+const increaseCount=()=>{
+  setCount(prev=>prev+1)
+  setCount(prev=>prev+1)
+  setCount(prev=>prev+1)
+  setCount(prev=>prev+1)
+}
+
+const [RenderCount,setRenderCount]=useState(0)
+
+useEffect(()=>{
+  setTimeout(()=>{
+    setRenderCount(RenderCount=>RenderCount+1) 
+  },2000)
+},[Count,RenderCount])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Count: {Count}</h1>
+      <h1>I've rendered {RenderCount} times</h1>
+      <h1>My name is Braison, I am a favor of God <br /> My favor color is {color} </h1>
+      <h3>I drive a {car.color} {car.brand} from {car.country} {car.model} </h3>
+      <button onClick={increaseCount}>Increment by 4</button>
+      <button onClick={changeColor}>Change color</button>
+      <button onClick={switchCar}>Change car</button>
     </>
   )
 }
